7a6ac7c2bc0f323857de70a1d8623a228885d005
tokufxug <tokufxug@gmail.com>
2024-01-10 22:20:07 +0900

    Initial Commit

---
+++ MultiOSSample/MultiOSSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiOSSampleApp.swift
+//  MultiOSSample
+//
+//  Created by Sadao Tokuyama on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiOSSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
